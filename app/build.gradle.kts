07c2c9035d09e80b0f9a8d210232b6192fa2d4ca
Mathew <matwong1212@gmail.com>
2023-11-02 13:53:25 -0700

    replaced realtime database with firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:20.3.0")
 
     implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/sfulounge/data/model/User.kt
+++ app/src/main/java/com/example/sfulounge/data/model/User.kt
@@ -9,4 +9,27 @@ data class User(
     var interests: List<String> = ArrayList(),
     var depthQuestions: List<DepthInfo> = ArrayList(),
     var photos: List<String> = ArrayList()
-)
+) {
+    companion object {
+        fun fromMap(map: Map<String, *>) = User(
+            userId = map["userId"] as String,
+            isProfileInitialized = map["isProfileInitialized"] as Boolean,
+            firstName = map["firstName"] as String?,
+            lastName = map["lastName"] as String?,
+            description = map["description"] as String?,
+            interests = (map["interests"] as List<*>).map { x -> x as String },
+            depthQuestions = (map["depthQuestions"] as List<*>).map { x -> x as DepthInfo },
+            photos = (map["photos"] as List<*>).map { x -> x as String }
+        )
+        fun toMap(user: User) = mapOf(
+            "userId" to user.userId,
+            "isProfileInitialized" to user.isProfileInitialized,
+            "firstName" to user.firstName,
+            "lastName" to user.lastName,
+            "description" to user.description,
+            "interests" to user.interests,
+            "depthQuestions" to user.depthQuestions.map { x -> DepthInfo.toMap(x) },
+            "photos" to user.photos
+        )
+    }
+}
