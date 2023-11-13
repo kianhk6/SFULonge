500667e8a8bb0bb678e4cb2cdaae198d0b2836be
natha <nka66@sfu.ca>
2023-11-13 00:45:15 -0800

    Initial setup for Images Activity
    Moved commented code to SetupImagesActivity.kt, created initial layout, and wired SetupBasicInfoActivity to SetupImagesActivity

--- app/src/main/java/com/example/sfulounge/ui/setup/SetupBasicInfoActivity.kt
+++ app/src/main/java/com/example/sfulounge/ui/setup/SetupBasicInfoActivity.kt
@@ -46,6 +46,7 @@ class SetupBasicInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
 
         Util.checkPermissions(this)
 
@@ -171,7 +172,7 @@ class SetupBasicInfoActivity : AppCompatActivity() {
      * wiring to activities
      */
     private fun onSaveUserSuccessful() {
-        val intent = Intent(this, SetupInterestsActivity::class.java)
+        val intent = Intent(this, SetupImagesActivity::class.java)
         interestsResultLauncher.launch(intent)
     }
 
