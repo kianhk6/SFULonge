24e446d04dce3540a3e97de9fe5c979ec8355934
Mathew <matwong1212@gmail.com>
2023-11-16 21:26:38 -0800

    fix typo

--- app/src/main/java/com/example/sfulounge/ui/chats/ChatsViewModelFactory.kt
+++ app/src/main/java/com/example/sfulounge/ui/chats/ChatsViewModelFactory.kt
@@ -5,13 +5,10 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.sfulounge.data.ChatRepository
 
 class ChatsViewModelFactory : ViewModelProvider.Factory {
-    private val _viewModel: ChatsViewModel by lazy {
-        ChatsViewModel(repository = ChatRepository())
-    }
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ChatsViewModel::class.java)) {
-            return _viewModel as T
+            return ChatsViewModel(repository = ChatRepository()) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
